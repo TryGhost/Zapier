8df6ad8cc576b99b9eaa02ccfd8cc87830c99edf
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-11-07 18:05:50 +0000

    extract resthook subscribe/unsubscribe functions

---
+++ hooks.js
@@ -0,0 +1,33 @@
+const subscribeHook = (eventName, z, bundle) => {
+    let data = {
+        target_url: bundle.targetUrl,
+        event: eventName
+    };
+
+    let options = {
+        url: '/webhooks/',
+        method: 'POST',
+        body: JSON.stringify(data)
+    };
+
+    // the data returned from webhooks endpoint is added directly to
+    // bundle.subscribeData
+    return z.request(options)
+        .then((response) => JSON.parse(response.content));
+};
+
+const unsubscribeHook = (z, bundle) => {
+    let hookId = bundle.subscribeData.id;
+
+    let options = {
+        url: `/webhooks/${hookId}/`,
+        method: 'DELETE'
+    };
+
+    return z.request(options);
+};
+
+module.exports = {
+    subscribeHook,
+    unsubscribeHook
+};
--- triggers/new_subscriber.js
+++ triggers/new_subscriber.js
@@ -1,31 +1,8 @@
-const subscribeHook = (z, bundle) => {
-    let data = {
-        target_url: bundle.targetUrl,
-        'event': 'subscriber.create'
-    };
+const _ = require('lodash');
+const hooks = require('../hooks');
 
-    let options = {
-        url: '/webhooks/',
-        method: 'POST',
-        body: JSON.stringify(data)
-    };
-
-    // the data returned from webhooks endpoint is added directly to
-    // bundle.subscribeData
-    return z.request(options)
-        .then((response) => JSON.parse(response.content));
-};
-
-const unsubscribeHook = (z, bundle) => {
-    let hookId = bundle.subscribeData.id;
-
-    let options = {
-        url: `/webhooks/${hookId}/`,
-        method: 'DELETE'
-    };
-
-    return z.request(options);
-};
+const subscribeHook = _.partial(hooks.subscribeHook, 'subscriber.create');
+const unsubscribeHook = hooks.unsubscribeHook;
 
 // triggers on new subscriber
 const getSubscriber = (z, bundle) => {
