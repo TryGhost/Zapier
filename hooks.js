e851ed01afb0e1f54ac03a0d694c44f5f7d5a899
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-11-27 13:31:43 +0000

    improve error responses for failed webhook subscriptions

--- hooks.js
+++ hooks.js
@@ -15,7 +15,21 @@ const subscribeHook = (eventName, z, bundle) => {
     // the data returned from webhooks endpoint is added directly to
     // bundle.subscribeData
     return z.request(options)
-        .then((response) => z.JSON.parse(response.content).webhooks[0]);
+        .then((response) => {
+            let json;
+
+            try {
+                json = z.JSON.parse(response.content);
+            } catch (e) {
+                throw new Error(`Webhook subscription failed: ${response.status}.`)
+            }
+
+            if (response.status !== 201) {
+                throw new Error(json.errors[0].message);
+            }
+
+            return json.webhooks[0];
+        });
 };
 
 const unsubscribeHook = (z, bundle) => {
