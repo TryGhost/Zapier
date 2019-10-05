4a90afcb82757edf542de219f380685cf6d88a21
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-10-05 08:51:55 +0100

    Updated order and help text of authentication fields

--- app/authentication.js
+++ app/authentication.js
@@ -54,18 +54,18 @@ module.exports = {
 
     fields: [
         {
-            key: 'adminApiUrl',
-            label: 'Admin API URL',
-            helpText: 'The Admin API URL copied from the Zapier Integration in your Ghost admin area.',
-            placeholder: 'https://yoursite.com',
+            key: 'adminApiKey',
+            label: 'Admin API Key',
+            helpText: 'Find these details Ghost Admin under <code>Integrations » Zapier</code>',
+            placeholder: '5c3e1182e79eace7f58c9c3b:7202e874ccae6f1ee6688bb700f356b672fb078d8465860852652037f7c7459ddbd2f2a6e9aa05a40b499ae20027d9f9ba2e5004aa9ab6510b90a5dac674cbc1',
             required: true,
             type: 'string'
         },
         {
-            key: 'adminApiKey',
-            label: 'Admin API Key',
-            helpText: 'The Admin API key copied from the Zapier Integration in your Ghost admin area.',
-            placeholder: '5c3e1182e79eace7f58c9c3b:7202e874ccae6f1ee6688bb700f356b672fb078d8465860852652037f7c7459ddbd2f2a6e9aa05a40b499ae20027d9f9ba2e5004aa9ab6510b90a5dac674cbc1',
+            key: 'adminApiUrl',
+            label: 'Admin API URL',
+            helpText: 'Find these details Ghost Admin under <code>Integrations » Zapier</code>',
+            placeholder: 'https://yoursite.com',
             required: true,
             type: 'string'
         }
