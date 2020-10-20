15ef263db130610bf5bfa14a01cfab79016ca2fe
Naz <nazargargol@gmail.com>
2020-10-21 12:53:01 +1300

    Added update_member create action
    closes #16
    
    - Includes the same update fields as the create action with a difference there are no required edited fields

--- app/creates/index.js
+++ app/creates/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     create_member: require('./create_member'),
+    update_member: require('./update_member'),
     create_post: require('./create_post'),
     create_subscriber: require('./create_subscriber'),
     delete_subscriber: require('./delete_subscriber')
