68aa51c9593c6e5f3c7a5a76b35c3a5450490413
naz <nazargargol@gmail.com>
2020-10-21 15:48:14 +1300

    Added update_member create action (#33)
    closes #16
    
    - Includes the same fields as the create action with a difference there are no required "edited" fields.
    - This action should mostly be paired with member search to fetch member's id needed for the action to function

---
+++ app/creates/update_member.js
@@ -0,0 +1,95 @@
+const {initAdminApi, versionCheck} = require('../lib/utils');
+
+const updateMember = async (z, bundle) => {
+    // Members was added in Ghost 3.0
+    let expectedVersion = '>=3.0.0';
+    let action = 'members';
+
+    const memberData = {
+        id: bundle.inputData.id,
+        name: bundle.inputData.name,
+        email: bundle.inputData.email,
+        note: bundle.inputData.note,
+        subscribed: bundle.inputData.subscribed
+    };
+
+    // Member Labels was added in Ghost 3.6
+    if (bundle.inputData.labels && bundle.inputData.labels.length > 0) {
+        expectedVersion = '>=3.6.0';
+        action = 'member labels';
+        memberData.labels = bundle.inputData.labels;
+    }
+
+    // Member Complimentary plan was in Ghost 3.36
+    if (bundle.inputData.comped !== undefined) {
+        expectedVersion = '>=3.36.0';
+        action = 'member complimentary plan';
+        memberData.comped = bundle.inputData.comped;
+    }
+
+    await versionCheck(expectedVersion, action, z, bundle);
+
+    const api = initAdminApi(z, bundle.authData, {version: 'v3'});
+
+    const queryParams = {};
+
+    return api.members.edit(memberData, queryParams);
+};
+
+module.exports = {
+    key: 'update_member',
+    noun: 'Member',
+
+    display: {
+        label: 'Update Member',
+        description: 'Updates a member (Only supported by Ghost 3.0.0 and later)',
+        important: true
+    },
+
+    operation: {
+        inputFields: [
+            {
+                key: 'id',
+                required: true,
+                label: 'Member',
+                dynamic: 'member_created.id.name',
+                search: 'member.id'
+            },
+            {key: 'name', required: false},
+            {key: 'email', required: false},
+            {key: 'note', required: false},
+            {
+                key: 'subscribed',
+                label: 'Subscribed to newsletter',
+                type: 'boolean',
+                helpText: 'If disabled, member will not receive newsletter emails',
+                required: false
+            },
+            {
+                key: 'labels',
+                required: false,
+                list: true,
+                helpText: 'Provide a list of labels to attach to the member ( >= Ghost 3.6)'
+            },
+            {
+                key: 'comped',
+                label: 'Complimentary premium plan',
+                type: 'boolean',
+                helpText: 'If enabled, member will be placed onto a free of charge premium subscription ( >= Ghost 3.36)'
+            }
+        ],
+
+        perform: updateMember,
+
+        sample: {
+            id: '5c9c9c8d51b5bf974afad2a4',
+            name: 'Test Member',
+            email: 'test@example.com',
+            comped: false,
+            subscribed: true,
+            labels: ['Zapier'],
+            created_at: '2019-03-28T10:06:05.862Z',
+            updated_at: '2019-03-28T10:06:05.862Z'
+        }
+    }
+};
---
+++ test/creates/update_member.test.js
@@ -0,0 +1,304 @@
+require('should');
+const nock = require('nock');
+
+const zapier = require('zapier-platform-core');
+
+// Use this to make test calls into your app:
+const App = require('../../index');
+const appTester = zapier.createAppTester(App);
+
+describe('Creates', function () {
+    describe('Update Member', function () {
+        let apiMock, authData;
+
+        beforeEach(function () {
+            apiMock = nock('http://zapier-test.ghost.io');
+            authData = {
+                adminApiUrl: 'http://zapier-test.ghost.io',
+                adminApiKey: '5c3e1182e79eace7f58c9c3b:7202e874ccae6f1ee6688bb700f356b672fb078d8465860852652037f7c7459ddbd2f2a6e9aa05a40b499ae20027d9f9ba2e5004aa9ab6510b90a5dac674cbc1'
+            };
+        });
+
+        afterEach(function () {
+            nock.cleanAll();
+        });
+
+        describe('with supported version', function () {
+            beforeEach(function () {
+                apiMock.get('/ghost/api/v2/admin/site/').reply(200, {
+                    site: {version: '3.0'}
+                });
+            });
+
+            it('updates a member', function () {
+                let bundle = Object.assign({}, {authData}, {
+                    inputData: {
+                        id: '5c9c9c8d51b5bf974afad2a4',
+                        name: 'Test Member',
+                        email: 'test@example.com',
+                        send_email: 'no',
+                        email_type: 'signup'
+                    }
+                });
+
+                apiMock.put('/ghost/api/v3/admin/members/5c9c9c8d51b5bf974afad2a4/', {
+                    members: [{
+                        name: 'Test Member',
+                        email: 'test@example.com'
+                    }]
+                }).reply(200, {
+                    members: [{
+                        id: '5c9c9c8d51b5bf974afad2a4',
+                        name: 'Test Member',
+                        email: 'test@example.com',
+                        created_at: '2019-10-03T11:54:10.123Z',
+                        updated_at: '2019-10-03T11:54:10.123Z'
+                    }]
+                });
+
+                return appTester(App.creates.update_member.operation.perform, bundle)
+                    .then((member) => {
+                        apiMock.isDone().should.be.true;
+
+                        member.id.should.equal('5c9c9c8d51b5bf974afad2a4');
+                        member.name.should.equal('Test Member');
+                        member.email.should.equal('test@example.com');
+                    });
+            });
+
+            it('has a friendly, halting validation error', function () {
+                let bundle = Object.assign({}, {authData}, {
+                    inputData: {
+                        id: '5c9c9c8d51b5bf974afad2a4',
+                        email: 'not_valid'
+                    }
+                });
+
+                apiMock.put('/ghost/api/v3/admin/members/5c9c9c8d51b5bf974afad2a4/', {
+                    members: [{
+                        email: 'not_valid'
+                    }]
+                }).reply(422, {
+                    errors: [{
+                        message: 'Validation error, cannot save subscriber.',
+                        context: 'Validation (isEmail) failed for email',
+                        type: 'ValidationError',
+                        details: null,
+                        property: null,
+                        help: null,
+                        code: null,
+                        id: '2749ebe0-5145-11e9-9864-f79cf99013d0'
+                    }]
+                });
+
+                return appTester(App.creates.update_member.operation.perform, bundle)
+                    .then(() => {
+                        true.should.equal(false);
+                    }, (err) => {
+                        err.name.should.eql('HaltedError');
+                        err.message.should.match(/Validation \(isEmail\) failed for email/);
+                    });
+            });
+
+            it('handles 500 errors with JSON error body', function () {
+                let bundle = Object.assign({}, {authData}, {
+                    inputData: {
+                        id: '5c9c9c8d51b5bf974afad2a4',
+                        name: 'Test Member',
+                        email: 'test@example.com'
+                    }
+                });
+
+                apiMock.put('/ghost/api/v3/admin/members/5c9c9c8d51b5bf974afad2a4/')
+                    .reply(500, {
+                        errors: [{
+                            message: 'Authorization failed',
+                            context: 'Unable to determine the authenticated user or integration. Check that cookies are being passed through if using session authentication.',
+                            type: 'NoPermissionError',
+                            details: null,
+                            property: null,
+                            help: null,
+                            code: null,
+                            id: '34950f70-5148-11e9-9864-f79cf99013d0'
+                        }]
+                    });
+
+                return appTester(App.creates.update_member.operation.perform, bundle)
+                    .then(() => {
+                        true.should.eql(false);
+                    }, (err) => {
+                        err.name.should.eql('RequestError');
+                        err.message.should.match(/Authorization failed/);
+                    });
+            });
+        });
+
+        describe('with supported version >=3.6', function () {
+            beforeEach(function () {
+                apiMock.get('/ghost/api/v2/admin/site/').reply(200, {
+                    site: {version: '3.6'}
+                });
+            });
+
+            it('updates member label', function () {
+                apiMock.get('/ghost/api/v2/admin/site/').reply(200, {
+                    site: {version: '3.6'}
+                });
+                let bundle = Object.assign({}, {authData}, {
+                    inputData: {
+                        id: '5c9c9c8d51b5bf974afad2a4',
+                        labels: ['Zapier']
+                    }
+                });
+
+                apiMock.put('/ghost/api/v3/admin/members/5c9c9c8d51b5bf974afad2a4/', {
+                    members: [{
+                        labels: ['Zapier']
+                    }]
+                }).reply(200, {
+                    members: [{
+                        id: '5c9c9c8d51b5bf974afad2a4',
+                        name: 'Test Member',
+                        email: 'test@example.com',
+                        labels: [
+                            {
+                                id: '5e425659230484605f002c57',
+                                name: 'Zapier',
+                                created_at: '2020-02-11T07:23:05.520Z',
+                                updated_at: '2020-02-11T07:23:05.520Z',
+                                slug: 'zapier'
+                            }
+                        ],
+                        created_at: '2019-10-03T11:54:10.123Z',
+                        updated_at: '2019-10-03T11:54:10.123Z'
+                    }]
+                });
+
+                return appTester(App.creates.update_member.operation.perform, bundle)
+                    .then((member) => {
+                        apiMock.isDone().should.be.true;
+
+                        member.id.should.equal('5c9c9c8d51b5bf974afad2a4');
+                        member.name.should.equal('Test Member');
+                        member.email.should.equal('test@example.com');
+                        member.labels.should.have.lengthOf(1);
+                    });
+            });
+        });
+
+        describe('with supported version >=3.36', function () {
+            beforeEach(function () {
+                apiMock.get('/ghost/api/v2/admin/site/').reply(200, {
+                    site: {version: '3.36'}
+                });
+            });
+
+            it('updates member comped flag', function () {
+                apiMock.get('/ghost/api/v2/admin/site/').reply(200, {
+                    site: {version: '3.36'}
+                });
+                let bundle = Object.assign({}, {authData}, {
+                    inputData: {
+                        id: '5c9c9c8d51b5bf974afad2a4',
+                        comped: true
+                    }
+                });
+
+                apiMock.put('/ghost/api/v3/admin/members/5c9c9c8d51b5bf974afad2a4/', {
+                    members: [{
+                        comped: true
+                    }]
+                }).reply(201, {
+                    members: [{
+                        id: '5c9c9c8d51b5bf974afad2a4',
+                        name: 'Test Member',
+                        email: 'test@example.com',
+                        comped: true,
+                        labels: [],
+                        created_at: '2019-10-03T11:54:10.123Z',
+                        updated_at: '2019-10-03T11:54:10.123Z'
+                    }]
+                });
+
+                return appTester(App.creates.update_member.operation.perform, bundle)
+                    .then((member) => {
+                        apiMock.isDone().should.be.true;
+
+                        member.id.should.equal('5c9c9c8d51b5bf974afad2a4');
+                        member.name.should.equal('Test Member');
+                        member.email.should.equal('test@example.com');
+                        member.comped.should.equal(true);
+                    });
+            });
+        });
+
+        describe('with unsupported version', function () {
+            beforeEach(function () {
+                apiMock.get('/ghost/api/v2/admin/site/').reply(200, {
+                    site: {version: '2.34'}
+                });
+            });
+
+            it('has a friendly, halting "unsupported version" error', function () {
+                let bundle = Object.assign({}, {authData}, {
+                    inputData: {
+                        name: 'Test Member',
+                        email: 'test@example.com'
+                    }
+                });
+
+                return appTester(App.creates.update_member.operation.perform, bundle)
+                    .then(() => {
+                        true.should.equal(false);
+                    }, (err) => {
+                        err.name.should.eql('HaltedError');
+                        err.message.should.match(/2\.34/);
+                    });
+            });
+        });
+
+        describe('with unsupported version <= 3.6', function () {
+            beforeEach(function () {
+                apiMock.get('/ghost/api/v2/admin/site/').reply(200, {
+                    site: {version: '3.0'}
+                });
+            });
+
+            it('has a friendly, halting "unsupported version" error for labels', function () {
+                let bundle = Object.assign({}, {authData}, {
+                    inputData: {
+                        name: 'Test Member',
+                        email: 'test@example.com',
+                        labels: ['Zapier']
+                    }
+                });
+
+                return appTester(App.creates.update_member.operation.perform, bundle)
+                    .then(() => {
+                        true.should.equal(false);
+                    }, (err) => {
+                        err.name.should.eql('HaltedError');
+                        err.message.should.match(/3\.0/);
+                    });
+            });
+
+            it('has a friendly, halting "unsupported version" error for comped flag', function () {
+                let bundle = Object.assign({}, {authData}, {
+                    inputData: {
+                        name: 'Test Member',
+                        email: 'test@example.com',
+                        comped: true
+                    }
+                });
+
+                return appTester(App.creates.update_member.operation.perform, bundle)
+                    .then(() => {
+                        true.should.equal(false);
+                    }, (err) => {
+                        err.name.should.eql('HaltedError');
+                        err.message.should.match(/3\.0/);
+                    });
+            });
+        });
+    });
+});
