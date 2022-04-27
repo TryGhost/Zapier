2f73a4842bdf76262ea2042d6c57418ef9aa2aef
Sam Lord <hello@samlord.me>
2022-04-27 10:50:39 +0100

    Remove "subscribers" concept
    refs: https://github.com/TryGhost/Toolbox/issues/238#issuecomment-1099812029
    
    The subscriber concept is from v2, and was superceded by members in v3. This API is being deleted completely in v5 so should be gone in next major of the Zapier integration.

--- app/triggers/index.js
+++ app/triggers/index.js
@@ -6,7 +6,5 @@ module.exports = {
     page_published: require('./page_published'),
     post_published: require('./post_published'),
     post_scheduled: require('./post_scheduled'),
-    subscriber_created: require('./subscriber_created'),
-    subscriber_deleted: require('./subscriber_deleted'),
     tag_created: require('./tag_created')
 };
